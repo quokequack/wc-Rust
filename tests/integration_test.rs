9473853fbaf2ec030e054f7460202b9bbd815735
Alaquoke <alaquoke@gmail.com>
2025-07-01 16:10:32 -0300

    fix(finalizando): consertando nome do comando

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -8,5 +8,5 @@ fn test_wc_output_lines() {
         .expect("Falha ao executar");
 
     let stdout = String::from_utf8(output.stdout).unwrap();
-    assert!(stdout.contains("       3 test1.txt")); // Exemplo de teste esperado
+    assert!(stdout.contains("       3 test1.txt")); 
 }
